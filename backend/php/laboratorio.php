bdbe1369e301786e4f4e0f8483acd08975f924dc
Caren302 <1721110574@utectulancingo.edu.mx>
2023-11-17 20:34:52 +0000

    actualizacion

--- backend/php/laboratorio.php
+++ backend/php/laboratorio.php
@@ -16,9 +16,11 @@
     $municipio = htmlspecialchars($_POST["municipio"]);
     $cp = htmlspecialchars($_POST["cp"]);
     $estado = htmlspecialchars($_POST["estado"]);
+    $correo = htmlspecialchars($_POST["correo"]);
+    $password = htmlspecialchars($_POST["password"]);
 
-    $insertar = "INSERT INTO laboratorio (num_sede, nombre, certificacion, numeroext, calle, colonia, municipio,cp,estado)
-    VALUES ('$num_sede', '$nombre', '$certificacion', '$numeroext','$calle','$colonia','$municipio','$cp','$estado')";
+    $insertar = "INSERT INTO laboratorio (num_sede, nombre, certificacion, numeroext, calle, colonia, municipio,cp,estado,correo,password)
+    VALUES ('$num_sede', '$nombre', '$certificacion', '$numeroext','$calle','$colonia','$municipio','$cp','$estado','$correo','$password')";
     
     $conectar = mysqli_connect($bd_host, $bd_user, $bd_pass, $bd_name, 3306);
 
@@ -29,8 +31,8 @@
     }
 
     mysqli_set_charset($conectar, "utf8");
-    $insertar = "INSERT INTO laboratorio (num_sede, nombre, certificacion, numeroext, calle, colonia, municipio,cp,estado)
-    VALUES ('$num_sede', '$nombre', '$certificacion', '$numeroext','$calle','$colonia','$municipio','$cp','$estado')";
+    $insertar = "INSERT INTO laboratorio (num_sede, nombre, certificacion, numeroext, calle, colonia, municipio,cp,estado,correo,password)
+    VALUES ('$num_sede', '$nombre', '$certificacion', '$numeroext','$calle','$colonia','$municipio','$cp','$estado','$correo','$password')";
 
     if ($resultado = mysqli_query($conectar, $insertar))
     {
