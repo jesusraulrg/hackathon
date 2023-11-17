09479de279eb7717ffd498318e00b93ee3037e79
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-11-17 12:27:10 +0000

    Update

--- frontend/static/login.php
+++ frontend/static/login.php
@@ -1,76 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>INICIAR SESION</title>
-</head>
-<body>
-    <?php
-    ob_start();
-    try
-    
-    {
-      
-        session_start();
-        
-        
-            $usuario= $_POST ["usuario"];
-            $password= $_POST ["password"];
-           
-             $_SESSION['matricula']=$usuario;
-            
-            MD5($password);
+<?php
+$correo = $_POST['correo'];
+$password = $_POST['password'];
 
-            $bd_host = "127.0.0.1";
-            $bd_user = "root";
-            $bd_pass = "";
-            $bd_name = "agendautec";
-            $bd_port = "3306";    
-            $conMySQL = mysqli_connect($bd_host, $bd_user, $bd_pass, $bd_name);
-            if (mysqli_connect_errno())
-            {
-                printf("ERROR: %u - %s", mysqli_connect_errno(), mysqli_connect_error());
-                exit();
-            }
-            $pass = MD5($password);
-            mysqli_set_charset($conMySQL, "utf8");
-            $sentenciaSQL = "SELECT * FROM alumnos_inscritos WHERE matricula = '$usuario' AND passwd = '$pass'";
-         
-            $log=htmlspecialchars(addslashes($usuario));
-            $pas=htmlspecialchars(addslashes($password));
-       
-            $result = mysqli_query($conMySQL,$sentenciaSQL);
+$conexion = new mysqli('', '', '', '');
 
-      
-            if ($result->num_rows > 0){
+if ($conexion->connect_error) {
+    die("Connection failed: " . $conexion->connect_error);
+}
 
+$sql = "SELECT * FROM usuarios WHERE correo = '$correo' AND password = '$password'";
+$resultado = $conexion->query($sql);
 
-                if($pass){
-                    header("location:contraseña.php");
-                }
-                else{
-                    header("Location:agenda.php");
+if ($resultado->num_rows > 0) {
+    // Iniciar sesión
+} else {
+    echo "Correo o contraseña incorrectos";
+}
 
-                }
-                
-              
-            }
-            else
-            {
-                print '<script language="JavaScript">';
-                print 'alert("Usuario o contraseña incorrecta");';
-                print '</script>';
-
-                require('inicio.html');
-            }
-    }
-    catch(Exception $e)
-    {
-        die("Error: ". $e->getMessage());
-    }
-    finally
-    { 
-        $conMySQL=null; 
-    }
-    ?>
-</body>
-</html>
+$conexion->close();
+?>
